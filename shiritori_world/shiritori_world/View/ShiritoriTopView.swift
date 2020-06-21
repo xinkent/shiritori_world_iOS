42d7c80ee19238d7d7574dbbe89817ab9c8bcf89
xinkent <xinkent1994@gmail.com>
2020-06-21 11:41:41 +0900

    Make ShiritoriTopView frame

--- shiritori_world/shiritori_world/View/ShiritoriTopView.swift
+++ shiritori_world/shiritori_world/View/ShiritoriTopView.swift
@@ -3,12 +3,38 @@ import SwiftUI
 struct ShiritoriTopView: View{
     @EnvironmentObject var shiritoriFetcher:ShiritoriFetcher
     @ObservedObject var vm = ShiritoriTopViewModel()
+    @State var longitude = 139.745451
+    @State var latitude = 35.658577
     
     var body: some View{
-        Text("ShiritoriTopView")
+        VStack{
+            ShiritoriAnswerView(vm:self.vm)
+            MapView(lon:self.$longitude, lat:self.$latitude)
+        }
     }
 }
 
+
+struct ShiritoriAnswerView:View{
+    @ObservedObject var vm:ShiritoriTopViewModel
+    var body: some View{
+        VStack{
+            Spacer().frame(height:50)
+            Text("現在のワード")
+                .font(.title)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+            Text("りんご")
+            Spacer().frame(height:50)
+            if(vm.isTurn){
+                TextField("解答入力",text: $vm.word )
+            }
+            Toggle("手番切り替え(開発用)", isOn: $vm.isTurn)
+            Spacer()
+        }.frame(maxWidth:.infinity)
+            .frame(height:300)
+    }
+}
 
 
 
