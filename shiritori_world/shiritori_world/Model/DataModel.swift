9553e9cc307cd8961745aefe20776782cc5bc3f4
yuta.yasumura <uta11190710@gmail.com>
2020-07-04 02:14:47 +0900

    Mod: modify CLGeo

--- shiritori_world/shiritori_world/Model/DataModel.swift
+++ shiritori_world/shiritori_world/Model/DataModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import MapKit
 
 struct User{
     var userID: String
@@ -19,5 +20,29 @@ struct ShiritoriWord:Identifiable{
     var userID: String
     var lat: Double
     var long: Double
+    var address: String?{
+        var address = ""
+        let location = CLLocation(latitude: 35.711271, longitude:139.797323)// default asakusa
+        
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            guard
+                let placemark = placemarks?.first, error == nil,
+                let administrativeArea = placemark.administrativeArea, // 都道府県
+                let locality = placemark.locality // 市区町村
+        //      let thoroughfare = placemark.thoroughfare, // 地名(丁目)
+        //      let subThoroughfare = placemark.subThoroughfare, // 番地
+        //      let postalCode = placemark.postalCode, // 郵便番号
+        //      let location = placemark.location // 緯度経度情報
+            else {
+                    return
+            }
+            print("placemark: \(placemark)")
+            print("prefecture: \(administrativeArea)")
+            print("locality: \(locality)")
+
+            address = administrativeArea + locality
+        }
+        return address
+    }
     var word: String
 }
--- shiritori_world/shiritori_world/ViewModel/ShiritoriFetcher.swift
+++ shiritori_world/shiritori_world/ViewModel/ShiritoriFetcher.swift
@@ -1,14 +1,17 @@
 import Firebase
 import SwiftUI
+import MapKit
 
 class ShiritoriFetcher: ObservableObject{
   @Published var shiritoriWords:[ShiritoriWord] = [] // UserIDに対応するshiritoriを取得する
   let db = Firestore.firestore()
+//  var address = ""
+    
   // TODO：取得DocumentIDの指定方法検討
   func fetchShiritori(documentID:String){
     db.collection("shiritori_test").document(documentID)
     .addSnapshotListener { documentSnapshot, error in
-        self.shiritoriWords = []
+        self.shiritoriWords = [] // 表示しりとりの初期化
          guard let document = documentSnapshot else {
           print("Error fetching document: \(error!)")
           return
@@ -17,15 +20,43 @@ class ShiritoriFetcher: ObservableObject{
           print("Document data was empty.")
           return
          }
-        print("Current data: \(data)")
-        let shiritoriWordList = data["shiritori_word"] as! [[String:Any]]
+        // print("Current data: \(data)") // debug
+        let shiritoriWordList = data["shiritori_word"] as! [[String:Any]] // fetchしたドキュメントからフィールドを抽出
+        
         for shiritoriWord in shiritoriWordList {
             let id = shiritoriWord["order"] as? Int
             let lat = shiritoriWord["lat"]! as? Double
             let long = shiritoriWord["long"]! as? Double
+//            print("lat: \(lat)")
+//            print("long: \(long)")
+            
+            let location = CLLocation(latitude: 35.711271, longitude: 139.797323)// default asakusa
+            // MapKitを用いた逆ジオコーディング
+//            var address = ""
+            CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+//                guard
+                    let placemark = placemarks?.first// error == nil,
+                let administrativeArea = placemark?.administrativeArea // 都道府県
+                let locality = placemark?.locality // 市区町村
+//                  let thoroughfare = placemark.thoroughfare, // 地名(丁目)
+//                  let subThoroughfare = placemark.subThoroughfare, // 番地
+//                  let postalCode = placemark.postalCode, // 郵便番号
+//                  let location = placemark.location // 緯度経度情報
+//                else {
+//                    return
+//                a}
+//                print("placemark: \(placemark)")
+//                print("prefecture: \(administrativeArea)")
+//                print("locality: \(locality)")
+
+//                self.address = administrativeArea! + locality!
+             }
+//            Thread.sleep(forTimeInterval: 3)
+//            print("address: \(self.address)")
             let userID = shiritoriWord["user_id"] as? String
             let word = shiritoriWord["word"] as? String
-            self.shiritoriWords.append(ShiritoriWord(id:id!, userID:userID!, lat:lat!, long:long!, word:word!))
+            
+            self.shiritoriWords.append(ShiritoriWord(id:id!, userID:userID!, lat:lat!, long:long!,  word:word!))
       }
     }
   } 
