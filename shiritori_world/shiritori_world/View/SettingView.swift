69406e35f34f7d75aecc6181643e3dd0da50156e
xinkent <xinkent1994@gmail.com>
2020-11-04 23:52:29 +0900

    change version to 1.0.5

--- shiritori_world/shiritori_world/View/SettingView.swift
+++ shiritori_world/shiritori_world/View/SettingView.swift
@@ -65,7 +65,7 @@ struct SettingView: View {
                     Image(systemName:"gear")
                         .foregroundColor(.black)
                     Spacer()
-                    Text("ver 1.0.4")
+                    Text("ver 1.0.5")
                     Spacer().frame(width: 15)
                         }
                 }
