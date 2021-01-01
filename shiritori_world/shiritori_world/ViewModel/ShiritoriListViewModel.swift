47b2d9089c72115e85b2c98660da0d0a961538e1
xinkent <xinkent1994@gmail.com>
2021-01-01 20:09:05 +0900

    add report function in list view

--- shiritori_world/shiritori_world/ViewModel/ShiritoriListViewModel.swift
+++ shiritori_world/shiritori_world/ViewModel/ShiritoriListViewModel.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 class ShiritoriListViewModel: ObservableObject {
-    @Published var beforeSent: Bool = false
+    @Published var updateBeforeSent: Bool = false
+    @Published var reportBeforeSent: Bool = false
     @Published var isSent: Bool = false
     @Published var isError: Bool = false
     @Published var order: Int = 0
@@ -9,10 +10,15 @@ class ShiritoriListViewModel: ObservableObject {
     // @Published var flag_name:String = "is_location_masked"
     // @Published var flag_value:Bool = true
     
-    func beforeSend(order:Int, is_masked:Bool){
+    func updateBeforeSend(order:Int, is_masked:Bool){
         self.order = order
         self.is_masked = is_masked
-        self.beforeSent = true
+        self.updateBeforeSent = true
+    }
+    
+    func reportBeforeSend(order:Int){
+        self.order = order
+        self.reportBeforeSent = true
     }
     
     func updateShiritoriFlag(sf:ShiritoriFetcher){
@@ -29,6 +35,23 @@ class ShiritoriListViewModel: ObservableObject {
             } else {
                 self.isSent = true
                 print("update shiritori succeeded")
+            }
+        }
+    }
+    
+    func reportShiritori(sf: ShiritoriFetcher){
+        print("call reportShiritori")
+        let flag_list = ["is_reported":true]
+        let data  = ["doc_id":sf.user.currentShiritoriID!, "shiritori_order":String(self.order), "flag_list":flag_list,
+        ] as [String : Any]
+        print(data)
+        sf.functions.httpsCallable("updateFlag").call(data){(result, error) in
+            if error != nil{
+                self.isError = true
+                print("report shiritori failed:\(String(describing: error))")
+            } else {
+                self.isSent = true
+                print("report shiritori succeeded")
             }
         }
     }
